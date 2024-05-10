ac1fb3d8f9132c8c1316b64318ed0d6af0a870d9
logic <logicfor2018@gmail.com>
2024-05-10 18:20:37 +0800

    init

--- docs/.vitepress/theme/components/foo.vue
+++ docs/.vitepress/theme/components/foo.vue
@@ -164,7 +164,7 @@
 				</div>
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
 					<dt class="text-base leading-7 text-gray-600">累计服务人数</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">6,00+</dd>
+					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">6,000+</dd>
 				</div>
 			</dl>
 		</div>
